ccacba2c91108be1a1bec7411df3bce6bc031ac3
itdaniher <itdaniher@gmail.com>
2020-08-13 17:50:46 +0000

    Merge branch 'id/fix-scripts' into 'master'
    fixup scripts
    
    See merge request radix-labs/monorepo!325
    
    GitOrigin-RevId: 2971bcc7afd9d33f563fff23393fed08f428a27c

--- scripts/format_diff.sh
+++ scripts/format_diff.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-git status | egrep "modified|new file" | tr " " "\n" | egrep "scala\$" | xargs -n 1 bazel query | xargs -n 1 -I {} bazel query "attr('srcs', {}, //...)" | sort | uniq | xargs -n 1 -I {} bazel run {}.format
+set -exu
+range=${1:-}
+changed_targets=$(git diff --name-only $range | grep scala$ | xargs -i bazel query {} --output package 2>/dev/null | sort -u | xargs -i bazel query "kind(scala_, //{}:*)" --output label_kind 2>/dev/null | sort -u | grep "scala_binary\ rule\|scala_library\ rule" | grep -v docker | awk '{print $3}')
+for target in $changed_targets
+    do bazel run $target.format 2> /dev/null 1>/dev/null
+done
