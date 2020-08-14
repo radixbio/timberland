b9e5cf703c4077b8b6d3a4e39259341bd77dde2a
DhashS <dhash@outlook.com>
2020-08-14 12:01:57 +0000

    Merge branch 'it/prism-serializers' into 'master'
    WIP Move serializers to persistence
    
    See merge request radix-labs/monorepo!329
    
    GitOrigin-RevId: 4d90c287b2eeda3f8d947f6248adcc4ef5d011c1

--- utils/helm/http4s/jvm/src/main/scala/com/radix/utils/helm/http4s/util.scala
+++ utils/helm/http4s/jvm/src/main/scala/com/radix/utils/helm/http4s/util.scala
@@ -4,7 +4,14 @@ object util {
   def getTokenFromEnvVars(): Option[String] = {
     val consulHttpToken = sys.env.get("CONSUL_HTTP_TOKEN")
     consulHttpToken match {
-      case None => sys.env.get("NOMAD_TOKEN")
+      case None =>
+        val consulHttpToken = System.getProperty("CONSUL_HTTP_TOKEN") //To allow for runtime overrides for develloping
+        if (consulHttpToken == null) {
+          sys.env.get("NOMAD_TOKEN")
+        }
+        else {
+          Some(consulHttpToken)
+        }
       case _ => consulHttpToken
     }
   }
