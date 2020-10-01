ee661bb1e1b44bb42d5134dd62754cef8efd9422
itdaniher <itdaniher@gmail.com>
2020-10-01 00:02:57 +0000

    Merge branch 'id/winjoin' into 'master'
    winjoin, again
    
    See merge request radix-labs/monorepo!383
    
    GitOrigin-RevId: e5b102e0e233e0b09d3c157baab7e5738115dde9

--- timberland/jvm/src/main/scala/com/radix/timberland/launch/dns.scala
+++ timberland/jvm/src/main/scala/com/radix/timberland/launch/dns.scala
@@ -1,15 +1,65 @@
 package com.radix.timberland.launch
 
-import java.nio.file.Path
-
-import cats.effect.{IO, Resource}
-import cats.implicits._
-import oshi.software.os.linux.LinuxOperatingSystem
+import cats.effect.IO
+import com.radix.timberland.util.RadPath
 
 object dns {
   trait DNS_method {
-    def up(): IO[Unit]
-    def down(): IO[Unit]
+    def set(): IO[Unit]
+    def reset(): IO[Unit]
+  }
+
+  case class Windows() extends DNS_method {
+    import com.github.tototoshi.csv.CSVReader
+    import com.profesorfalken.jpowershell
+    import scala.io.Source
+
+    val defaultDnsPath: os.Path = RadPath.persistentDir / ".defaultDNS"
+    private val ps = jpowershell.PowerShell.openSession()
+
+    private def getDefaultInterfaceIndex(): String = {
+      val psPipeline =
+        "Get-NetIPInterface -AddressFamily IPv4 | select ifIndex, InterfaceMetric | ConvertTo-Csv -NoTypeInformation"
+      val outputCsvNoHeader = ps.executeCommand(psPipeline).getCommandOutput
+      CSVReader.open(Source.fromString(outputCsvNoHeader)).allWithHeaders().head("ifIndex")
+    }
+    private def getInterfaces(): List[Map[String, String]] = {
+      val psPipeline =
+        "Get-DnsClientServerAddress | Select-Object ElementName, {$_.ServerAddresses}, AddressFamily, InterfaceIndex | ConvertTo-Csv -NoTypeInformation"
+      val outputCsvNoHeader = ps.executeCommand(psPipeline).getCommandOutput
+      CSVReader.open(Source.fromString(outputCsvNoHeader)).allWithHeaders
+    }
+
+    def set(): IO[Unit] =
+      for {
+        defaultInterfaceIndex <- IO(getDefaultInterfaceIndex())
+        // get ipv4 servers on the default interface
+        currentServers = getInterfaces()
+          .filter(line => line.get("AddressFamily").contains("2") & line("InterfaceIndex") == defaultInterfaceIndex)
+          .head("$_.ServerAddresses")
+        defaultDNSExists = os.exists(defaultDnsPath)
+        _ = scribe.info(s"current servers: $currentServers; default exists: $defaultDNSExists")
+        _ <- if (!currentServers.contains("127.0.0.1")) for {
+          // don't update DNS if 127.0.0.1 is already present
+          _ <- IO(os.write(defaultDnsPath, currentServers))
+          setServerResp = ps.executeCommand(
+            s"Set-DnsClientServerAddress -InterfaceIndex $defaultInterfaceIndex -ServerAddresses '127.0.0.1 $currentServers'"
+          )
+        } yield setServerResp
+        else IO(scribe.info("DNS already setup, doing nothing."))
+      } yield ()
+
+    def reset(): IO[Unit] =
+      for {
+        defaultInterfaceIndex <- IO(getDefaultInterfaceIndex())
+        defaultDNSExists = os.exists(defaultDnsPath)
+        // reset to defaults if the dns file doesn't exist
+        resettingCommand = if (defaultDNSExists) s"-ServerAddresses '${os.read(defaultDnsPath)}'"
+        else "-ResetServerAddresses"
+        resetDefaultDNSServers <- IO(
+          ps.executeCommand(s"Set-DnsClientServerAddress -InterfaceIndex $defaultInterfaceIndex $resettingCommand")
+        )
+      } yield ()
   }
 
   case class Netplan() extends DNS_method {
@@ -25,7 +75,7 @@ object dns {
                                    |          - 127.0.0.1
                                    |""".stripMargin
 
-    def up(): IO[Unit] = {
+    def set(): IO[Unit] = {
       os.write.over(netplan_file, netplan_config)
       val result = os.proc(Seq("/usr/bin/sudo", "netplan", "apply")).call()
       if (result.exitCode != 0) {
@@ -33,7 +83,7 @@ object dns {
       }
       IO.unit
     }
-    def down(): IO[Unit] = {
+    def reset(): IO[Unit] = {
       os.remove(netplan_file)
       val result = os.proc(Seq("/usr/bin/sudo", "netplan", "apply")).call()
       if (result.exitCode != 0) {
@@ -48,7 +98,7 @@ object dns {
                                |DNS1=127.0.0.1
                                |""".stripMargin
 
-    def up(): IO[Unit] = {
+    def set(): IO[Unit] = {
       val config_text = os.read(ifcfg_file)
       if (!config_text.contains(ifcfg_line)) {
         os.write.append(ifcfg_file, ifcfg_line)
@@ -59,7 +109,7 @@ object dns {
       }
       IO.unit
     }
-    def down(): IO[Unit] = {
+    def reset(): IO[Unit] = {
       val config_text = os.read(ifcfg_file)
       os.write.over(ifcfg_file, config_text.replaceAll(ifcfg_line, ""))
       val result = os.proc(Seq("/usr/bin/sudo", "systemctl", "restart", "network")).call()
@@ -74,7 +124,7 @@ object dns {
     private val dhclient_file = os.root / "etc" / "dhcp" / "dhclient.conf"
     private val dhclient_line = "prepend domain-name-servers 127.0.0.1;"
 
-    def up(): IO[Unit] = {
+    def set(): IO[Unit] = {
       val config_text = os.read(dhclient_file)
       if (!(config_text.contains(dhclient_line))) {
         os.write.over(dhclient_file, dhclient_line + "\n" + config_text)
@@ -85,7 +135,7 @@ object dns {
       }
       IO.unit
     }
-    def down(): IO[Unit] = {
+    def reset(): IO[Unit] = {
       val config_text = os.read(dhclient_file)
       os.write.over(dhclient_file, config_text.replaceAll(dhclient_line, ""))
       val result = os.proc(Seq("/usr/bin/sudo", "systemctl", "restart", "network")).call()
@@ -141,7 +191,7 @@ object dns {
       }
     }
 
-    def up(): IO[Unit] = {
+    def set(): IO[Unit] = {
       val config_lines = os.read(resolved_file).split("\n").toList
       val add_keys = (add_key(dns_key, dns_val) _) compose (add_key(dom_key, dom_val) _)
 
@@ -154,7 +204,7 @@ object dns {
       IO.unit
     }
 
-    def down(): IO[Unit] = {
+    def reset(): IO[Unit] = {
       val config_text = os.read(resolved_file).split("\n").toList
       val rm_keys = (rem_key(dns_key, dns_val) _) compose (rem_key(dom_key, dom_val) _)
 
@@ -172,7 +222,7 @@ object dns {
     private val resolvconf_file = os.root / "etc" / "resolv.conf"
     private val resolvconf_line = "nameserver 127.0.0.1"
 
-    def up(): IO[Unit] = {
+    def set(): IO[Unit] = {
       val conf_text = os.read(resolvconf_file)
       // We expect that iff our nameserver is present, /etc/resolv.conf is write-protected.
       if (!(conf_text.contains(resolvconf_line))) {
@@ -195,7 +245,7 @@ object dns {
       IO.unit
     }
 
-    def down(): IO[Unit] = {
+    def reset(): IO[Unit] = {
       val result = os.proc(Seq("/usr/bin/sudo", "chattr", "-i", resolvconf_file.toString)).call()
       if (result.exitCode != 0) {
         throw new RuntimeException("Could not unprotect /etc/resolv.conf")
@@ -209,8 +259,8 @@ object dns {
   }
 
   case class NetworkManager() extends DNS_method {
-    def up(): IO[Unit] = {
-      dns.Resolved().up()
+    def set(): IO[Unit] = {
+      dns.Resolved().set()
       val ret = os.proc(Seq("/usr/bin/sudo", "systemctl", "restart", "NetworkManager")).call()
       if (!(ret.exitCode == 0)) {
         println(s"NetworkManager config error codes: ${ret.exitCode}")
@@ -218,8 +268,8 @@ object dns {
       Thread.sleep(1000) // DNS will still fail for a second as NM comes back up
       IO.unit
     }
-    def down(): IO[Unit] = {
-      dns.Resolved().down()
+    def reset(): IO[Unit] = {
+      dns.Resolved().reset()
       val ret = os.proc(Seq("/usr/bin/sudo", "systemctl", "restart", "NetworkManager")).call()
       if (!(ret.exitCode == 0)) {
         println(s"NetworkManager config error codes: ${ret.exitCode}")
@@ -229,11 +279,11 @@ object dns {
   }
 
   case class NoMethod() extends DNS_method {
-    def up(): IO[Unit] = {
+    def set(): IO[Unit] = {
       scribe.error("No valid DNS method found! (up)")
       IO.unit
     }
-    def down(): IO[Unit] = {
+    def reset(): IO[Unit] = {
       scribe.error("No valid DNS method found! (down)")
       IO.unit
     }
@@ -243,7 +293,8 @@ object dns {
   private val dhclient_string = "generated by /usr/sbin/dhclient-script"
   private val resolved_string = "This file is managed by man:systemd-resolved(8). Do not edit."
   def identify_DNS_control: DNS_method = {
-    val resolvconf_text = os.read(os.root / "etc" / "resolv.conf")
+    val resolvconf_path = os.root / "etc" / "resolv.conf"
+    val resolvconf_text = if (os.exists(resolvconf_path)) os.read(resolvconf_path) else ""
     if (resolvconf_text.contains(NM_string)) {
       scribe.info("Detected NetworkManager DNS control.")
       NetworkManager()
@@ -253,6 +304,9 @@ object dns {
     } else if (resolvconf_text.contains(resolved_string)) {
       scribe.info("Detected systemd-resolve DNS control")
       Resolved()
+    } else if (System.getProperty("os.name").toLowerCase.contains("windows")) {
+      scribe.info("Detected Windows DNS control")
+      Windows()
     } else {
       scribe.info("Did not successfully detect DNS control!")
       NoMethod()
@@ -260,9 +314,9 @@ object dns {
   }
 
   def up(): IO[Unit] = {
-    identify_DNS_control.up()
+    identify_DNS_control.set()
   }
   def down(): IO[Unit] = {
-    identify_DNS_control.down()
+    identify_DNS_control.reset()
   }
 }
