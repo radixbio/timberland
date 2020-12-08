7c147e5b39c4d53957bb689c8550558e67436639
DhashS <dhash@outlook.com>
2020-12-08 07:17:29 +0000

    Merge branch 'it/jmhalgs' into 'master'
    It/jmhalgs
    
    See merge request radix-labs/monorepo!460
    
    GitOrigin-RevId: 2e30128aed30ea14508bf27cab61eea42f26da38

--- scripts/format_codebase.sh
+++ scripts/format_codebase.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -exu
-bazel query "kind(scala_library, //...) + kind(scala_binary, //...)" | egrep "^//" | grep -v "//3rdparty" | egrep -v "\.binary\$" | xargs -n 1 -I {} bazel run {}.format
+bazel query "kind(scala_library, //...) + kind(scala_binary, //...)" | egrep "^//" | grep -v "//3rdparty" | egrep -v "\.binary\$" | egrep -v ".*jmh_codegen" | egrep -v ".*jmh_generator" | egrep -v ".*-jmh"  | xargs -n 1 -I {} bazel run {}.format
+
