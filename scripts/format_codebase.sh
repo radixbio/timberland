47e89dc66d1c784138de00aceb650546232cab52
DhashS <dhash@outlook.com>
2021-01-07 21:24:53 +0000

    Merge branch 'ds/make-format-faster' into 'master'
    save on bazel reinvocation cost
    
    See merge request radix-labs/monorepo!475
    
    GitOrigin-RevId: 89b339073b873a03d5f813608e7bd98228585386

--- scripts/format_codebase.sh
+++ scripts/format_codebase.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -exu
-bazel query "kind(scala_library, //...) + kind(scala_binary, //...)" | egrep "^//" | grep -v "//3rdparty" | egrep -v "\.binary\$" | egrep -v ".*jmh_codegen" | egrep -v ".*jmh_generator" | egrep -v ".*-jmh"  | xargs -n 1 -I {} bazel run {}.format
+bazel query "kind(scala_library, //...) + kind(scala_binary, //...)" | egrep "^//" | grep -v "//3rdparty" | egrep -v "\.binary\$" | egrep -v ".*jmh_codegen" | egrep -v ".*jmh_generator" | egrep -v ".*-jmh" | xargs -I {} echo "{}.format" | xargs -I {} bazel run "{}"
 
